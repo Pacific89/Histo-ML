d294e38a5425e27d616ec4bf69f1c4eab89c26b8
Pacific89 <philipp.faulhammer@medunigraz.at>
2022-03-31 13:18:52 +0200

    type after reshape

--- contrastive.py
+++ contrastive.py
@@ -169,8 +169,8 @@ class ContrastiveExtractor():
     def get_patch(self, coords, wsi_path, patch_size=256):
 
         patch = self.wsi.read_region(tuple(coords), level=0, size=(patch_size, patch_size)).convert('RGB')
-        print(patch.resize((224,224)))
         patch = np.reshape(patch.resize((224,224)), (3,224,224))
+        print(type(patch))
 
 
         # plt.figure()
