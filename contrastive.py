e3cecba81265fc462e56e6ba359caffc9375f2aa
Pacific89 <philipp.faulhammer@medunigraz.at>
2022-03-31 13:16:22 +0200

    up

--- contrastive.py
+++ contrastive.py
@@ -159,7 +159,7 @@ class ContrastiveExtractor():
         all_feat_frame = pd.DataFrame([])
         chunked_list = list(more_itertools.chunked(all_coords, self.batch_size))
 
-        for count, (batch, coords) in tqdm(enumerate(chunked_list)):
+        for coord_subset in tqdm(chunked_list):
 
             patch_array = self.create_patch_dict(coord_subset)
             frame = self.extract_features(patch_array)
@@ -169,6 +169,7 @@ class ContrastiveExtractor():
     def get_patch(self, coords, wsi_path, patch_size=256):
 
         patch = self.wsi.read_region(tuple(coords), level=0, size=(patch_size, patch_size)).convert('RGB')
+        print(type(patch))
         patch = np.reshape(patch.resize((224,224)), (3,224,224))
 
 
