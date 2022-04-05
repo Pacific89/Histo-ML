8e0ec8d6fa0b58c8c751a85271f3f55a93a0f07e
Pacific89 <philipp.faulhammer@medunigraz.at>
2022-04-05 09:47:32 +0200

    typo

--- contrastive.py
+++ contrastive.py
@@ -50,9 +50,9 @@ class ContrastiveExtractor():
         print("Initialized")
 
     def collate_features(self, batch):
-	img = torch.cat([item[0] for item in batch], dim = 0)
-	coords = np.vstack([item[1] for item in batch])
-	return [img, coords]
+        img = torch.cat([item[0] for item in batch], dim = 0)
+        coords = np.vstack([item[1] for item in batch])
+        return [img, coords]
 
     def get_wsi_path(self):
 
