4d2ac7ba9366b169bbc95f6f0211d7c72c01603f
Pacific89 <philipp.faulhammer@medunigraz.at>
2022-05-18 10:42:33 +0200

    typo

--- main.py
+++ main.py
@@ -202,7 +202,7 @@ if __name__ == "__main__":
     if args.save_h5:
         combined_features, combined_targets_class, combined_targets_reg = get_combined_data_subset(args)
     if os.path.isdir(args.data_path):
-        features_path = "combined_features.h5")
+        features_path = "combined_features.h5"
         targets_class_path = "combined_hrd_targets.h5"
         targets_reg_path = "combined_gis_targets.h5"
 
